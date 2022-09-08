da79a23cf674d2438bafc25490d5b994ad1715af
gashug <geoffrey.gachuiri98@gmail.com>
2022-09-08 15:26:47 -0700

    Hello World Practice

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-
 /**
  * main - print the size of various types
  *
- * Description: prints the size of various types on the computer it is compiled 
+ * Description: prints the size of various types on the computer it is compiled
  *
  * Return: Always 0 (Success)
  */
