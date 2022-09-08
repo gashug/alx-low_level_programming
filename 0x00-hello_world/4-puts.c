7dadc4e06aaebb74fc313be6f0292eba33b74fae
gashug <geoffrey.gachuiri98@gmail.com>
2022-09-08 14:49:13 -0700

    Hello World Practice

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
+	
 	return (0);
 }
