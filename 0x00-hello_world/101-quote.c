9f2fdac4a596549a58040544b7aa565303600bdc
gashug <geoffrey.gachuiri98@gmail.com>
2022-09-12 13:42:38 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print triple combos
+ *
+ * Description: print triple digit combos
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (0);
+}
