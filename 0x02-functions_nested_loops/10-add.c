5b77968f4ef1a3b3881c634e01883a0e7879d9b5
gashug <geoffrey.gachuiri98@gmail.com>
2022-09-14 09:46:51 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,11 @@
 
 /**
  * add - entry point
- *
  * @x: first number
- *
  * @y: second number
  * Description: prints _putchar
  *
- * Return: 0 (Success)
+ * Return: result returned after adding (Success)
  */
 int add(int x, int y)
 {
