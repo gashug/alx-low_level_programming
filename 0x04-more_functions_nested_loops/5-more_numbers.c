4ddeda5a0e6649d742abcf86292c027d0ee5ac67
gashug <geoffrey.gachuiri98@gmail.com>
2022-09-15 13:23:58 -0700

    first_six

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,16 @@
 
 void more_numbers(void)
 {
-	/*int i;*/
+	int i;
 	char c;
 
-	for (c = 0; c <= 14; c++)
+	for (i = 0; i <= 10; i++)
 	{
-		/*if (c > 9)*/
-		_putchar((c / 10) + '0');
-		_putchar((c / 10) + '0');
-	}
-	_putchar('\n');
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+				_putchar((c / 10) + '0');
+				_putchar((c / 10) + '0');
+		}
+		_putchar('\n');
 }
