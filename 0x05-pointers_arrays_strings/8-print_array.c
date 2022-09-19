c58aa2bf2c9f5ef11699fd2ef1573016ba403dbf
gashug <geoffrey.gachuiri98@gmail.com>
2022-09-19 10:11:26 -0700

    Mandatory section

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_array - prints n elements of an array
  * @a: array of integers
  * @n: number of items to print
  * Return: void
  */
-
 void print_array(int *a, int n)
 {
 	int i, count;
