9c7b4c8755833efbaae8e5e177ca51f2ef9d3d5b
gashug <geoffrey.gachuiri98@gmail.com>
2022-09-28 04:41:29 -0700

    Manddatory_section

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,12 @@ int _strlen_recursion(char *s)
 	int n = 1;
 	
 	if (*s == '\0')
+	{
 		return (0);
-
+	}
 	if (*s != '\0')
+	{
 		n = n + _strlen_recursion(s + 1);
-
+	}
 	return (n);
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,5 +11,5 @@ int factorial(int n)
 	else if (n <= 1)
 		return (1);
 	else
-		return(n * factorial(n - 1));
+		return (n * factorial(n - 1));
 }
