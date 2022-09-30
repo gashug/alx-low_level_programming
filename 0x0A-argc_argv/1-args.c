c9fd31d440fd57807b08c72781ff8208d9001cad
gashug <geoffrey.gachuiri98@gmail.com>
2022-09-30 14:44:56 -0700

    Mandatory section

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * 
+ * @argc: counts no. of arguments
+ * @argv: pointer to arguments
+ * Return: (0) Success.
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
